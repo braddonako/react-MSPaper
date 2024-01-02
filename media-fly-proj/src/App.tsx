c180c30912754b061a22fd1e5ae3699218f4e385
braddonako <bdonakowski@noxhealth.com>
2024-01-02 10:35:42 -0700

    proj created with typescript

---
+++ media-fly-proj/src/App.tsx
@@ -0,0 +1,22 @@
+// Store == global state
+
+import React from "react";
+
+// Action => Tells redux what to do to the state
+// increment  
+// decrement
+
+
+// Reducers
+
+
+const App = () => {
+  return (
+    <div>
+      <h2>Redux</h2>
+      {/* <Counter /> */}
+    </div>
+  )
+}
+
+export default App;
